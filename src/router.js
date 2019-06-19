0f8b6858b74db4c82c5dd7cd19e13c4e9d4db4a9
Austin <austinpdx@gmail.com>
2019-06-19 15:51:03 -0700

    remove work page for now, just use client as slug

--- src/router.js
+++ src/router.js
@@ -11,23 +11,24 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
+        path: '/',
+        name: 'home',
+        component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/AboutPage.vue')
+        path: '/about',
+        name: 'about',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ './views/AboutPage.vue')
     },
     {
-      path: '/work/:clientSlug',
-      name: 'client',
-    //   component: Projects
-      component: () => import(/* webpackChunkName: "work" */ './views/WorkPage.vue')
+        // path: '/work/:clientSlug', if later need a work page
+        path: '/:clientSlug',
+        name: 'client',
+        // component: Projects
+        component: () => import(/* webpackChunkName: "about" */ './views/WorkPage.vue')
     }
   ]
 })
--- src/views/WorkPage.vue
+++ src/views/WorkPage.vue
@@ -1,17 +1,19 @@
 <template>
     <div class="work-page">
-        test
+        <Header/>
         <Client/>
     </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import Header from '@/components/Header.vue'
 import Client from '@/components/Client.vue'
 
 export default {
     name: 'work-page',
     components: {
+        Header,
         Client
     }
 }
