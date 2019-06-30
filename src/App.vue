3a8cee24ef3c2028b47bbf18c59a38736fab7530
Austin <austinpdx@gmail.com>
2019-06-30 15:03:32 -0700

    update resume

--- src/App.vue
+++ src/App.vue
@@ -60,14 +60,6 @@ export default {
     },
 
     methods: {
-        // preloadAudio(url) {
-        //     var audio = new Audio();
-        //     // once this file loads, it will call loadedAudio()
-        //     // the file will be kept by the browser as cache
-        //     audio.addEventListener('canplaythrough', loadedAudio, false);
-        //     audio.src = url;
-        // },
-
         play (audio) {
             audio.isPlaying = true;
             audio.file.play();
@@ -81,16 +73,6 @@ export default {
         setIcons(bool) {
             this.lightIcons = bool;
         }
-    },
-
-    mounted() {
-        const preload = new Audio(require('./assets/fats.mp3'));
-        preload.load();
-        // preloadAudio();
-
-        // const { audio } = this.$refs;
-        // audio.addEventListener('canplaythrough', isAppLoaded, false);
-        // audio.load();
     }
 }
 </script>
